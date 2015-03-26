d19fb68ef36799f4914abfe3b83d907a10a7d0e6
onur <onuradsay@gmail.com>
2015-03-26 13:16:02 +0200

    Added getAlpha tests

--- test/test-alpha.js
+++ test/test-alpha.js
@@ -18,6 +18,27 @@ describe('test-alpha.js >>', function() {
     });
   });
 
+  describe('getAlpha()', function() {
+    it('should return 1 for #123456', function() {
+      expect(coco.getAlpha('#123456')).to.equal(1);
+    });
+    it('should return 1 for rgb(0, 0, 0)', function() {
+      expect(coco.getAlpha('rgb(0, 0, 0)')).to.equal(1);
+    });
+    it('should return 1 for rgba(0, 0, 0)', function() {
+      expect(coco.getAlpha('rgba(0, 0, 0)')).to.equal(1);
+    });
+    it('should return 0 for rgba(0, 0, 0, 0)', function() {
+      expect(coco.getAlpha('rgba(0, 0, 0, 0)')).to.equal(0);
+    });
+    it('should return 0.5 for hsla(0, 0%, 0%, 0.5)', function() {
+      expect(coco.getAlpha('hsla(0, 0%, 0%, 0.5)')).to.equal(0.5);
+    });
+    it('should return 0.75 for hsva(0, 0%, 0%, .75)', function() {
+      expect(coco.getAlpha('hsva(0, 0%, 0%, .75)')).to.equal(0.75);
+    });
+  });
+
   describe('css4 supported', function() {
     before(function() {
       coco.supportCss4();
@@ -25,6 +46,7 @@ describe('test-alpha.js >>', function() {
     after(function() {
       coco.unsupportCss4();
     });
+
     describe('removeAlpha()', function() {
       it('should return #123456', function() {
         expect(coco.removeAlpha('#123456')).to.equal('#123456');
@@ -37,6 +59,41 @@ describe('test-alpha.js >>', function() {
       });
       it('should return #123', function() {
         expect(coco.removeAlpha('#1234')).to.equal('#123');
+      });
+    });
+
+    describe('getAlpha()', function() {
+      it('should return 1 for #123456', function() {
+        expect(coco.getAlpha('#123456')).to.equal(1);
+      });
+      it('should return 1 for #123', function() {
+        expect(coco.getAlpha('#123')).to.equal(1);
+      });
+      // hex8
+      it('should return 1 for #123456ff', function() {
+        expect(coco.getAlpha('#123456ff')).to.equal(1);
+      });
+      it('should return 0.75 for #123456c0', function() {
+        expect(coco.getAlpha('#123456c0')).to.equal(0.75);
+      });
+      it('should return 0.47 for #12345678', function() {
+        expect(coco.getAlpha('#12345678')).to.equal(0.47);
+      });
+      it('should return 0 for #12345600', function() {
+        expect(coco.getAlpha('#12345600')).to.equal(0);
+      });
+      // hex4
+      it('should return 1 for #123f', function() {
+        expect(coco.getAlpha('#123f')).to.equal(1);
+      });
+      it('should return 0.8 for #123c', function() {
+        expect(coco.getAlpha('#123c')).to.equal(0.8);
+      });
+      it('should return 0.27 for #1234', function() {
+        expect(coco.getAlpha('#1234')).to.equal(0.27);
+      });
+      it('should return 0 for #1230', function() {
+        expect(coco.getAlpha('#1230')).to.equal(0);
       });
     });
   });
