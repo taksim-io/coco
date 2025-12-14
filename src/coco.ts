4396e0368b51d8d92f1956108f6f334f2afd8379
Onur Adsay <onuradsay@gmail.com>
2025-12-14 03:15:11 +0300

    Remove hue2* methods

--- src/coco.ts
+++ src/coco.ts
@@ -1,36 +1,7 @@
-import {
-  convert,
-  hue2hex,
-  hue2hsl,
-  hue2hsv,
-  hue2lab,
-  hue2lch,
-  hue2oklab,
-  hue2oklch,
-  hue2rgb,
-  hue2xyz,
-  parse,
-  serialize,
-} from "./core/convert";
+import { convert, parse, serialize } from "./core/convert";
 import { createCoco } from "./core/factory";
 import { namedColors } from "./data/named-colors";
 
 const coco = createCoco();
 
-export {
-  coco,
-  convert,
-  createCoco,
-  hue2hex,
-  hue2hsl,
-  hue2hsv,
-  hue2lab,
-  hue2lch,
-  hue2oklab,
-  hue2oklch,
-  hue2rgb,
-  hue2xyz,
-  namedColors,
-  parse,
-  serialize,
-};
+export { coco, convert, createCoco, namedColors, parse, serialize };
